2c60761027984049474580fdd38f9735c6d1509a
SunHaonan <903257958@qq.com>
2025-04-03 11:12:32 +0800

    bmp280_i2c_sw “海拔算法优化

--- bmp280_i2c_sw/driver/bmp280.c
+++ bmp280_i2c_sw/driver/bmp280.c
@@ -2,12 +2,6 @@
 #include "delay.h"
 #include "bmp280.h"
 
-#define CONST_PF 	0.1902630958
-#define FIX_TEMP 	25
-
-#define FILTER_NUM	5
-#define FILTER_A	0.1f
-
 /* BMP280私有数据结构体 */
 typedef struct {
 	I2CDev_t i2c;	// 软件I2C设备
@@ -37,11 +31,11 @@ BMP280CalibrationData_t	calibration_data;
 static int __bmp280_configure(BMP280Dev_t *dev, BMP280Config_t *config);
 static int __bmp280_get_id(BMP280Dev_t *dev, uint8_t *id);
 static int __bmp280_get_raw_data(BMP280Dev_t *dev, int32_t *pressure_raw, int32_t *temperature_raw);
-static int __bmp280_temperature_compensate(BMP280Dev_t *dev, int32_t *temperature_raw, float *temperature);
-static int __bmp280_altitude_calculate(BMP280Dev_t *dev, float *pressure, float *altitude);
+static int __bmp280_temperature_compensate(BMP280Dev_t *dev, int32_t *temperature_raw);
+static int __bmp280_pressure_compensate(BMP280Dev_t *dev, int32_t *pressure_raw);
+static int __bmp280_altitude_calculate(BMP280Dev_t *dev);
 static int __bmp280_get_data(BMP280Dev_t *dev);
 static int __bmp280_deinit(BMP280Dev_t *dev);
-static void __presssure_filter(float* in, float* out);
 
 /******************************************************************************
  * @brief	初始化BMP280
@@ -77,6 +71,7 @@ int bmp280_init(BMP280Dev_t *dev)
 
 	/* 往复位寄存器写入给定值 */
 	priv_data->i2c.write_reg(&priv_data->i2c, BMP280_ADDRESS, BMP280_RESET_REG, BMP280_RESET_VALUE);
+	delay_ms(50);
 	
 	/* 设置过采样因子、保持时间和滤波器分频因子 */
 	BMP280Config_t	bmp280_config_structure;
@@ -167,13 +162,35 @@ static int __bmp280_get_raw_data(BMP280Dev_t *dev, int32_t *pressure_raw, int32_
 }
 
 /******************************************************************************
+ * @brief	BMP280温度值补偿
+ * @param	dev    			:   BMP280Dev_t 结构体指针
+ * @param	temperature_raw	:   原始温度数据
+ * @return	0, 表示成功, 其他值表示失败
+ ******************************************************************************/
+static int __bmp280_temperature_compensate(BMP280Dev_t *dev, int32_t *temperature_raw)
+{
+	if (!dev || !dev->init_flag)
+        return -1;
+
+	double var1, var2;
+	
+	var1 = (((double)*temperature_raw) / 16384.0 - ((double)calibration_data.dig_t1) / 1024.0) * ((double)calibration_data.dig_t2);
+	var2 = 	((((double)*temperature_raw) / 131072.0 - ((double)calibration_data.dig_t1) / 8192.0) *
+			(((double)*temperature_raw) / 131072.0 - ((double) calibration_data.dig_t1) / 8192.0)) * 
+			((double)calibration_data.dig_t3);
+	calibration_data.t_fine = (int32_t)(var1 + var2);
+	dev->data.temperature = (var1 + var2) / 5120.0;
+
+	return 0;
+}
+
+/******************************************************************************
  * @brief	BMP280气压值补偿
  * @param	dev    			:   BMP280Dev_t 结构体指针
  * @param	pressure_raw	:   原始气压数据
- * @param	pressure		:   补偿后的气压数据（单位：hPa）
  * @return	0, 表示成功, 其他值表示失败
  ******************************************************************************/
-static int __bmp280_pressure_compensate(BMP280Dev_t *dev, int32_t *pressure_raw, float *pressure)
+static int __bmp280_pressure_compensate(BMP280Dev_t *dev, int32_t *pressure_raw)
 {
 	if (!dev || !dev->init_flag)
         return -1;
@@ -188,7 +205,7 @@ static int __bmp280_pressure_compensate(BMP280Dev_t *dev, int32_t *pressure_raw,
 	var1 = (1.0 + var1 / 32768.0) * ((double)calibration_data.dig_p1);
 	if (var1 == 0.0)
 	{
-		*pressure = 0;
+		dev->data.pressure = 0;
 		return -2;
 	}
 	p = 1048576.0 - (double)(*pressure_raw);
@@ -196,31 +213,7 @@ static int __bmp280_pressure_compensate(BMP280Dev_t *dev, int32_t *pressure_raw,
 	var1 = ((double)calibration_data.dig_p9) * p * p / 2147483648.0;
 	var2 = p * ((double)calibration_data.dig_p8) / 32768.0;
 	p = p + (var1 + var2 + ((double)calibration_data.dig_p7)) / 16.0;
-	*pressure = p / 100.0;
-
-	return 0;
-}
-
-/******************************************************************************
- * @brief	BMP280温度值补偿
- * @param	dev    			:   BMP280Dev_t 结构体指针
- * @param	temperature_raw	:   原始温度数据
- * @param	temperature		:   补偿后的温度数据（单位：摄氏度）
- * @return	0, 表示成功, 其他值表示失败
- ******************************************************************************/
-static int __bmp280_temperature_compensate(BMP280Dev_t *dev, int32_t *temperature_raw, float *temperature)
-{
-	if (!dev || !dev->init_flag)
-        return -1;
-
-	double var1, var2;
-	
-	var1 = (((double)*temperature_raw) / 16384.0 - ((double)calibration_data.dig_t1) / 1024.0) * ((double)calibration_data.dig_t2);
-	var2 = 	((((double)*temperature_raw) / 131072.0 - ((double)calibration_data.dig_t1) / 8192.0) *
-			(((double)*temperature_raw) / 131072.0 - ((double) calibration_data.dig_t1) / 8192.0)) * 
-			((double)calibration_data.dig_t3);
-	calibration_data.t_fine = (int32_t)(var1 + var2);
-	*temperature = (var1 + var2) / 5120.0;
+	dev->data.pressure = p / 100.0;
 
 	return 0;
 }
@@ -232,12 +225,17 @@ static int __bmp280_temperature_compensate(BMP280Dev_t *dev, int32_t *temperatur
  * @param	altitude	:   海拔（单位：m）
  * @return	0, 表示成功, 其他值表示失败
  ******************************************************************************/
-static int __bmp280_altitude_calculate(BMP280Dev_t *dev, float *pressure, float *altitude)
+static int __bmp280_altitude_calculate(BMP280Dev_t *dev)
 {
 	if (!dev || !dev->init_flag)
         return -1;
-	
-	*altitude = (pow((1015.7f / *pressure), CONST_PF) - 1.0f) * (FIX_TEMP + 273.15f) / 0.0065f;
+
+	const float P0 = 1013.250f;   						// 标准海平面气压（单位：hPa）
+	const float T_K = dev->data.temperature + 273.15f;	// 转换为开尔文温度
+
+	/* Hypsometric公式 */
+	float ratio =  dev->data.pressure / P0;
+	dev->data.altitude = (powf(ratio, 0.1903f) - 1.0f) * T_K / 0.0065f;
 
 	return 0;
 }
@@ -254,21 +252,16 @@ static int __bmp280_get_data(BMP280Dev_t *dev)
 
 	int32_t pressure_raw = 0;
 	int32_t temperature_raw = 0;
-	float pressure_unfiltered;
 	
 	/* 获取传感器数据 */
 	__bmp280_get_raw_data(dev, &pressure_raw, &temperature_raw);
 
 	/* 数据补偿并保存 */
-	__bmp280_temperature_compensate(dev, &temperature_raw, &dev->data.temperature);
-	__bmp280_pressure_compensate(dev, &pressure_raw, &pressure_unfiltered);
-
-	/* 限幅平均滤波 */
-	__presssure_filter(&pressure_unfiltered, &dev->data.pressure);
-	dev->data.pressure = (float)pressure_unfiltered;
+	__bmp280_temperature_compensate(dev, &temperature_raw);
+	__bmp280_pressure_compensate(dev, &pressure_raw);
 
 	/* 海拔计算 */
-	__bmp280_altitude_calculate(dev, &dev->data.pressure, &dev->data.altitude);
+	__bmp280_altitude_calculate(dev);
 
 	return 0;
 }
@@ -295,53 +288,4 @@ static int __bmp280_deinit(BMP280Dev_t *dev)
 	dev->init_flag = false;	// 修改初始化标志
 
     return 0;
-}
-
-/******************************************************************************
- * @brief	限幅平均滤波法
- * @param	in	:	输入
- * @param	out	:   输出
- * @return	无
- ******************************************************************************/
-static void __presssure_filter(float* in, float* out)
-{	
-	uint8_t i = 0;
-	static float filter_buf[FILTER_NUM] = {0.0};
-	double filter_sum = 0.0;
-	uint8_t cnt = 0;	
-	float deta;
-
-	if (filter_buf[i] == 0.0f)
-	{
-		filter_buf[i] = *in;
-		*out = *in;
-		if (++i >= FILTER_NUM)
-		{
-			i = 0;
-		}
-	} 
-	else 
-	{
-		if (i)
-		{
-			deta = *in - filter_buf[i - 1];
-		}
-		else
-		{
-			deta = *in - filter_buf[FILTER_NUM - 1];
-		}
-		if (fabs(deta) < FILTER_A)
-		{
-			filter_buf[i] = *in;
-			if (++i >= FILTER_NUM)
-			{
-				i = 0;
-			}
-		}
-		for (cnt = 0; cnt < FILTER_NUM; cnt++)
-		{
-			filter_sum += filter_buf[cnt];
-		}
-		*out = filter_sum / FILTER_NUM;
-	}
 }
